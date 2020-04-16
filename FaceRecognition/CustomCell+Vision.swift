9ba0d53d2a5a81762b809acb947a2aab0f1d68e3
SUBNARA <subhashini.narayanaswamy@daimler.com>
2020-04-16 21:15:37 +0530

    Code refactoring

--- FaceRecognition/CustomCell+Vision.swift
+++ FaceRecognition/CustomCell+Vision.swift
@@ -36,30 +36,26 @@ extension CustomCell {
         guard let baseVC = delegate as? ViewController else {
             return UIView()
         }
-        if let navBarHeight = baseVC.navigationController?.navigationBar.frame.size.height {
-
-            let imageScaledHeight = self.getScaledHeight(image: image)
-            let yTransform = -imageScaledHeight - self.photoImageView.frame.height / 2 + imageScaledHeight / 2
-            let transformFlip = CGAffineTransform.init(scaleX: 1, y: -1).translatedBy(x: 0, y: yTransform)
-            let transformScale = CGAffineTransform.identity.scaledBy(x: self.frame.width, y: imageScaledHeight)
-            let convertedRect = rect.applying(transformScale).applying(transformFlip)
-            
-            let faceBox = UIView()
-            faceBox.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-            faceBox.layer.borderWidth = 2
-            faceBox.layer.cornerRadius = 8
-            faceBox.frame = convertedRect
-            faceBox.backgroundColor = UIColor(white: 1.0, alpha: 0.3)
-            faceBox.layer.transform = CATransform3DMakeScale(0, 0, 0)
-            
-            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
-                faceBox.layer.transform = CATransform3DMakeScale(1, 1, 1)
-            }, completion: nil)
-            return faceBox
-        }
         
-        return UIView()
+        let imageScaledHeight = self.getScaledHeight(image: image)
+        let yTransform = -imageScaledHeight - self.photoImageView.frame.height / 2 + imageScaledHeight / 2
+        let transformFlip = CGAffineTransform.init(scaleX: 1, y: -1).translatedBy(x: 0, y: yTransform)
+        let transformScale = CGAffineTransform.identity.scaledBy(x: self.frame.width, y: imageScaledHeight)
+        let convertedRect = rect.applying(transformScale).applying(transformFlip)
         
+        let faceBox = UIView()
+        faceBox.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        faceBox.layer.borderWidth = 2
+        faceBox.layer.cornerRadius = 8
+        faceBox.frame = convertedRect
+        faceBox.backgroundColor = UIColor(white: 1.0, alpha: 0.3)
+        faceBox.layer.transform = CATransform3DMakeScale(0, 0, 0)
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
+            faceBox.layer.transform = CATransform3DMakeScale(1, 1, 1)
+        }, completion: nil)
+        return faceBox
+                
     }
     
     func getScaledHeight(image: UIImage) -> CGFloat {
