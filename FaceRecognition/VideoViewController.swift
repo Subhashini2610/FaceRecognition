9890e1b86d4ca6f8f7075c07eb83c633f1d10830
SUBNARA <subhashini.narayanaswamy@daimler.com>
2020-04-16 21:35:20 +0530

    Adding tab bar to integrate videos section into the app

---
+++ FaceRecognition/VideoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  VideoViewController.swift
+//  FaceRecognition
+//
+//  Created by Narayanaswamy, Subhashini (623) on 16/04/20.
+//  Copyright © 2020 Narayanaswamy, Subhashini (623). All rights reserved.
+//
+
+import UIKit
+
+class VideoViewController: UIViewController {
+    
+}
